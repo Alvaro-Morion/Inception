c6b9f536992f960e1072211b61f69940bec9d051
Alvaro Morion de eusebio <amorion-@c1r2s4.42madrid.com>
2023-11-26 10:38:22 +0100

    wordpress

--- srcs/requirements/mariadb/tools/dbcreator.sh
+++ srcs/requirements/mariadb/tools/dbcreator.sh
@@ -1,10 +1,13 @@
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS '{SQL_DATABASE}';"
-mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@localhost IDENTIFIED BY ${SQL_PASSWORD};"
-mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE} TO ${SQL_USER}@localhost IDENTIFIED BY ${SQL_PASSWORD};"
-mysql -e "ALTER USER root@localhost IDENTIFIED BY ${SQL_ROOT_PASSWORD};"
-mysql -e "FLUSH PRIVILEGES"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-exec mysqld_safe
+mysql --user=root --password=$SQL_ROOT_PASSWORD << EOF
+	create database if not exists '$SQL_DATABASE';
+	create user if not exists '$SQL_USER' identified by '$SQL_PASSWORD';
+	grant all privileges on '$SQL_DATABASE'.* to '$SQL_USER';
+	flush privileges;
+	alter user 'root'@'localhost' identified by '$SQL_ROOT_PASSWORD';
+	shutdown;
+EOF
+
+mysqld_safe
 
